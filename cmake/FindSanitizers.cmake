arsenm/sanitizers-cmake

commit ef2848366f7ec681a8f05a955ad70df7a305b8a4

Update cmake api calls for Ubuntu Focal proper opertion.
Add mandatory Sanitizers_FOUND flag to the module.

Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

cmake/FindSanitizers.cmake
@@ -26,8 +26,8 @@
 # link against the sanitizers.
 option(SANITIZE_LINK_STATIC "Try to link static against sanitizers." Off)
 
-
-
+# Highlight this module has been loaded.
+set(Sanitizers_FOUND TRUE)
 
 set(FIND_QUIETLY_FLAG "")
 if (DEFINED Sanitizers_FIND_QUIETLY)
@@ -38,9 +38,6 @@ find_package(ASan ${FIND_QUIETLY_FLAG})
 find_package(TSan ${FIND_QUIETLY_FLAG})
 find_package(MSan ${FIND_QUIETLY_FLAG})
 find_package(UBSan ${FIND_QUIETLY_FLAG})
-
-
-
 
 function(sanitizer_add_blacklist_file FILE)
     if(NOT IS_ABSOLUTE ${FILE})

cmake/sanitize-helpers.cmake
@@ -75,6 +75,7 @@ endfunction ()
 
 # Helper function to check compiler flags for language compiler.
 function (sanitizer_check_compiler_flag FLAG LANG VARIABLE)
+
     if (${LANG} STREQUAL "C")
         include(CheckCCompilerFlag)
         check_c_compiler_flag("${FLAG}" ${VARIABLE})
@@ -97,6 +98,7 @@ function (sanitizer_check_compiler_flag FLAG LANG VARIABLE)
                 " - Failed (Check not supported)")
         endif ()
     endif()
+
 endfunction ()
 
 
@@ -167,11 +169,10 @@ function (sanitizer_add_flags TARGET NAME PREFIX)
         return()
     endif()
 
-    # Set compile- and link-flags for target.
-    set_property(TARGET ${TARGET} APPEND_STRING
-        PROPERTY COMPILE_FLAGS " ${${PREFIX}_${TARGET_COMPILER}_FLAGS}")
-    set_property(TARGET ${TARGET} APPEND_STRING
-        PROPERTY COMPILE_FLAGS " ${SanBlist_${TARGET_COMPILER}_FLAGS}")
-    set_property(TARGET ${TARGET} APPEND_STRING
-        PROPERTY LINK_FLAGS " ${${PREFIX}_${TARGET_COMPILER}_FLAGS}")
+    separate_arguments(flags_list UNIX_COMMAND "${${PREFIX}_${TARGET_COMPILER}_FLAGS} ${SanBlist_${TARGET_COMPILER}_FLAGS}")
+    target_compile_options(${TARGET} PRIVATE ${flags_list})
+
+    separate_arguments(flags_list UNIX_COMMAND "${${PREFIX}_${TARGET_COMPILER}_FLAGS}")
+    target_link_options(${TARGET} PRIVATE ${flags_list})
+
 endfunction ()

commit 4e942e5bc55c8d96de7f95544a10ecbc0c744b13

Add minimum required CMake version
You get an error message like this (from CMake 3.7) if you try to use
sanitizers-cmake with CMake versions less than 3.13:

    CMake Error at cmake/sanitizers/cmake/sanitize-helpers.cmake:176 (target_link_options):
      Unknown CMake command "target_link_options".
    Call Stack (most recent call first):
      cmake/sanitizers/cmake/FindASan.cmake:61 (sanitizer_add_flags)
      cmake/sanitizers/cmake/FindSanitizers.cmake:94 (add_sanitize_address)
      cmake/sanitizers/cmake/FindSanitizers.cmake:107 (add_sanitizers)
      CMakeLists.txt:52 (add_sanitizers_per_dir_targets)

References: #33
Fixes: ef2848366f7e ("Update cmake api calls for Ubuntu Focal proper opertion.")
Signed-off-by: Alexander Dahl <ada@thorsis.com>

cmake/FindSanitizers.cmake
@@ -22,6 +22,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+cmake_minimum_required(VERSION 3.13)	# target_link_options()
+
 # If any of the used compiler is a GNU compiler, add a second option to static
 # link against the sanitizers.
 option(SANITIZE_LINK_STATIC "Try to link static against sanitizers." Off)